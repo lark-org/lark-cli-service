a63c28961cd58fd3b28b25b195de2abc72cb8b1c
virgoone <me@virgo.one>
2021-11-16 19:27:02 +0800

    feat: 🎸 构建时增加extra信息输出和performance记录

--- src/polyfills/appLog.js
+++ src/polyfills/appLog.js
@@ -67,76 +67,12 @@ export const prettyLog = (() => {
     pretty(title, text, 'danger')
   }
 
-  /**
-   *
-   * @param {*} 性能指标输出
-   * @param {*} "网页重定向的耗时"、"DNS查询的耗时"、 "TCP连接的耗时"、"下载服务端返回数据的时间"、"http请求总耗时"、"dom加载完成的时间"、"页面load的总耗时"
-   */
-  const performance = () => {
-    setTimeout(() => {
-      let performance = window.performance
-      if (performance) {
-        let time = performance.getEntriesByType('navigation')[0]
-        let resource = 0
-        time || (resource = (time = performance.timeOrigin).navigationStart)
-        let detail = [
-          {
-            key: 'Redirect',
-            desc: '网页重定向的耗时',
-            value: time.redirectEnd - time.redirectStart
-          },
-          {
-            key: 'AppCache',
-            desc: '检查本地缓存的耗时',
-            value: time.domainLookupStart - time.fetchStart
-          },
-          {
-            key: 'DNS',
-            desc: 'DNS查询的耗时',
-            value: time.domainLookupEnd - time.domainLookupStart
-          },
-          {
-            key: 'TCP',
-            desc: 'TCP连接的耗时',
-            value: time.connectEnd - time.connectStart
-          },
-          {
-            key: 'Waiting(TTFB)',
-            desc: '从客户端发起请求到接收到响应的时间 / Time To First Byte',
-            value: time.responseStart - time.requestStart
-          },
-          {
-            key: 'Content Download',
-            desc: '下载服务端返回数据的时间',
-            value: time.responseEnd - time.responseStart
-          },
-          {
-            key: 'HTTP Total Time',
-            desc: 'HTTP请求总耗时',
-            value: time.responseEnd - time.requestStart
-          },
-          {
-            key: 'DOMContentLoaded',
-            desc: 'DOM加载完成的时间',
-            value: time.domContentLoadedEventEnd - resource
-          },
-          {
-            key: 'Loaded',
-            desc: '页面加载的总耗时',
-            value: time.loadEventEnd - resource
-          }
-        ]
-        console.table(detail)
-      }
-    }, 0)
-  }
   return {
     primary,
     success,
     info,
     warning,
-    danger,
-    performance
+    danger
   }
 })()
 export function logVersion() {
@@ -144,15 +80,9 @@ export function logVersion() {
   if (__DEV__ || !APP_LOG) {
     return
   }
-  try {
-    prettyLog.success('App Version', `v${VERSION}`)
-    prettyLog.primary('构建时间：', BUILD_TIME)
 
-    window.onload = () => {
-      prettyLog.performance()
-    }
-    window.prettyLog = prettyLog
-    window.__APP__ = {
+  try {
+    window.__Lark__ = {
       name: APP_NAME,
       env: APP_ENV,
       version: `v${VERSION}`,
@@ -160,6 +90,78 @@ export function logVersion() {
       buildTime: BUILD_TIME,
       sha: RELEASE
     }
+
+    prettyLog.success('App Version', `v${VERSION}`)
+    prettyLog.primary('构建时间：', BUILD_TIME)
+
+    window.onload = () => {
+      /**
+       *
+       * @param {*} 性能指标输出
+       * @param {*} "网页重定向的耗时"、"DNS查询的耗时"、 "TCP连接的耗时"、"下载服务端返回数据的时间"、"http请求总耗时"、"dom加载完成的时间"、"页面load的总耗时"
+       */
+      const performance = () => {
+        setTimeout(() => {
+          let performance = window.performance
+          if (performance) {
+            let time = performance.getEntriesByType('navigation')[0]
+            let resource = 0
+            time || (resource = (time = performance.timeOrigin).navigationStart)
+            let detail = [
+              {
+                key: 'Redirect',
+                desc: '网页重定向的耗时',
+                value: time.redirectEnd - time.redirectStart
+              },
+              {
+                key: 'AppCache',
+                desc: '检查本地缓存的耗时',
+                value: time.domainLookupStart - time.fetchStart
+              },
+              {
+                key: 'DNS',
+                desc: 'DNS查询的耗时',
+                value: time.domainLookupEnd - time.domainLookupStart
+              },
+              {
+                key: 'TCP',
+                desc: 'TCP连接的耗时',
+                value: time.connectEnd - time.connectStart
+              },
+              {
+                key: 'Waiting(TTFB)',
+                desc: '从客户端发起请求到接收到响应的时间 / Time To First Byte',
+                value: time.responseStart - time.requestStart
+              },
+              {
+                key: 'Content Download',
+                desc: '下载服务端返回数据的时间',
+                value: time.responseEnd - time.responseStart
+              },
+              {
+                key: 'HTTP Total Time',
+                desc: 'HTTP请求总耗时',
+                value: time.responseEnd - time.requestStart
+              },
+              {
+                key: 'DOMContentLoaded',
+                desc: 'DOM加载完成的时间',
+                value: time.domContentLoadedEventEnd - resource
+              },
+              {
+                key: 'Loaded',
+                desc: '页面加载的总耗时',
+                value: time.loadEventEnd - resource
+              }
+            ]
+            window.__Lark__.performance = detail
+            console.table(detail)
+          }
+        }, 0)
+      }
+      performance()
+    }
+    window.prettyLog = prettyLog
   } catch (error) {}
 }
 logVersion()
